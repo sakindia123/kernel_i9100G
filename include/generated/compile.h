400e413c63b811c1a4f5f25a84fe1a281b7f9a44
Sarthak Acharya <sa6915@gmail.com>
2012-03-26 20:05:33 +0530

    Misc

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
 /* This file is auto generated, version cranium-testkeys */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#cranium-testkeys SMP PREEMPT Mon Mar 26 08:17:37 IST 2012"
-#define LINUX_COMPILE_TIME "08:17:37"
+#define UTS_VERSION "#cranium-testkeys SMP PREEMPT Mon Mar 26 16:28:14 IST 2012"
+#define LINUX_COMPILE_TIME "16:28:14"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "sarthak"
 #define LINUX_COMPILE_DOMAIN "(none)"
